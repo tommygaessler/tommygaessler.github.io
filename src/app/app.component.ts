0ee085a7525ce192d8b868744faeb4d025648b18
Tommy Gaessler <tommy.gaessler@gmail.com>
2017-07-04 15:03:35 -0600

    fixed mobile hamburger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const $;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    $('li a, .navbar-toggle').on('click', function () {
+      $('.navbar-toggle').toggleClass('active');
+    });
+
+    $('.navbar-collapse a').on('click', function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+  }
 }
