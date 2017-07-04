710ed25b1d9dbe04350e52f53de2fcc244dbc3c0
Tommy Gaessler <tommy.gaessler@gmail.com>
2017-07-04 13:34:41 -0600

    updated readme and added cname record for deployed site

---
+++ tommygaessler.github.io/README.md
@@ -0,0 +1,3 @@
+# [tommygaessler](http://tommygaessler.com)
+
+My personal website built with Angular2 & TypeScript!
---
+++ tommygaessler.github.io/assets/projects/projects.ts
@@ -0,0 +1,92 @@
+export var projects:Object = {
+  trip_charge: {
+    name: 'Trip Charge',
+    image: 'assets/images/projects/tripcharge.png',
+    description: 'Find charging stations for your electric car along a route, or closest to you!',
+    languages: 'Swift / GO',
+    deployed: 'https://itunes.apple.com/us/app/trip-charge-map-stations-along/id1186368321?mt=8',
+    github: 'https://github.com/tommygaessler/trip-charge',
+    type: 'app'
+  },
+  sitekite: {
+    name: 'SiteKite',
+    image: 'assets/images/projects/sitekite.png',
+    description: 'A full-stack app using GitHub OAuth that builds portfolio websites for developers to showcase who they are, and what they’ve built.',
+    languages: 'Node.js / Express / Nunjucks / PostgreSQL / HTML / CSS',
+    deployed: 'http://sitekite.co',
+    github: 'https://github.com/tommygaessler/sitekite',
+    type: 'web'
+  },
+  fundsplash: {
+    name: 'Fundsplash',
+    image: 'assets/images/projects/fundsplash.png',
+    description: 'A crowd funding app for <a href="https://unsplash.com/" target="_blank">Unsplash</a> photographers!',
+    languages: 'php / JavaScript / jQuery / HTML / Sass',
+    deployed: 'http://fundsplash.herokuapp.com',
+    github: 'https://github.com/tommygaessler/fundsplash',
+    type: 'web'
+  },
+  kamstrafam: {
+    name: 'KamstraFam',
+    image: 'assets/images/projects/kamstrafam.png',
+    description: 'A custom WordPress site/blog I built for a client’s mission trip to Prague, Czech Republic.',
+    languages: 'php / SQL / jQuery / HTML / Sass',
+    deployed: 'http://kamstrafam.com',
+    github: 'https://github.com/tommygaessler/kamstrafam',
+    type: 'web'
+  },
+  senior_capstone: {
+    name: 'Senior Capstone',
+    image: 'assets/images/projects/seniorcapstone.png',
+    description: 'Senior capstone project reflecting my life at Regis Jesuit High School.',
+    languages: 'php / jQuery / HTML / Sass',
+    deployed: 'https://seniorcapstone.herokuapp.com/',
+    github: 'https://github.com/tommygaessler/regis-jesuit-senior-capstone',
+    type: 'web'
+  },
+  kids247: {
+    name: 'Kids247',
+    image: 'assets/images/projects/kids247.png',
+    description: 'A custom WordPress website/blog I built for a clients non-profit that helps kids and families in times of divorce!',
+    languages: 'php / SQL / jQuery / HTML / Sass',
+    deployed: 'https://kids247.org/',
+    github: 'https://github.com/tommygaessler/kids247',
+    type: 'web'
+  },
+  habi_pets: {
+    name: 'Habi Pets',
+    video: '_hMKpE4CvmY',
+    description: 'A landing page and video I made for a company in the Telluride Venture Accelerator!',
+    languages: 'HTML / CSS',
+    deployed: 'http://habipets.com',
+    github: 'https://github.com/tommygaessler/habipets',
+    type: 'web/video'
+  },
+  pfw: {
+    name: 'PFW',
+    image: 'assets/images/projects/pfw.png',
+    description: 'A website I built for a clients non-profit that helps families prepare for when loved ones pass away!',
+    languages: 'php / HTML / CSS',
+    deployed: 'http://preparingtofinishwell.org',
+    github: 'https://github.com/tommygaessler/preparingtofinishwell',
+    type: 'web'
+  },
+  used_calculators: {
+    name: 'Used Calculators',
+    image: 'assets/images/projects/usedcalculators.png',
+    description: 'A Christmas themed e-commerce website I made to sell calculators!',
+    languages: 'php / HTML / CSS',
+    deployed: 'http://usedcalculators.co',
+    github: 'https://github.com/tommygaessler/usedcalculators',
+    type: 'web'
+  },
+  suit_up_denver: {
+    name: 'Suit Up Denver',
+    video: 'nHNMkOq5xSQ',
+    description: 'A website I built for a clients non-profit that donates suits to people in need of professional business attire!',
+    languages: 'php / HTML / CSS',
+    deployed: 'https://suitupdenver.herokuapp.com',
+    github: 'https://github.com/tommygaessler/suitupdenver',
+    type: 'web/video'
+  }
+}
