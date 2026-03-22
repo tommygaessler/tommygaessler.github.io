220654c7567514607babff56777a0aab8343ed34
Tommy Gaessler <tommy.gaessler@gmail.com>
2026-03-21 19:00:30 -0600

    updated fonts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,30 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, Router, NavigationEnd } from '@angular/router';
 import { Canonical } from './canonical';
 import { filter } from 'rxjs/operators';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBookBible } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faXTwitter, faMedium, faYoutube, faUnsplash, faAppStoreIos, faNpm, faGithub, faStackOverflow, faZoom } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('tommygaessler');
+
+  faBookBible = faBookBible;
+  faLinkedin = faLinkedin;
+  faXTwitter = faXTwitter;
+  faMedium = faMedium;
+  faYoutube = faYoutube;
+  faUnsplash = faUnsplash;
+  faAppStoreIos = faAppStoreIos;
+  faGithub = faGithub;
+  faNpm = faNpm;
+  faStackOverflow = faStackOverflow;
+  faZoom = faZoom;
 
   constructor(private router: Router, private canonical: Canonical) {
     this.router.events
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faZoom } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+
+  faZoom = faZoom;
 
 }
